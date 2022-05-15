2217c7085cad43aa371bb4130a3f5972a2ce3634
Sagi Polaczek <56922146+SagiPolaczek@users.noreply.github.com>
2022-05-15 17:33:10 +0300

    TAC progress

--- TAC/tac.py
+++ TAC/tac.py
@@ -1,23 +1,56 @@
+import numpy
+import torch
+from collections.abc import Callable
+
+import utils
 
 
 
 def tac_algorithm(training_set, validation_set, base_image):
+    """
 
-
-
-    pass
-
-
-def nearest_odd_square(d: int):
+    """
     
+
     pass
 
-def pad(feature_vector, k, method='random'):
+def feature_vector_to_kernel(feature_vector: numpy.ndarray):
     """
-    Adding padding to the feature vector.
-    Methods: random noise, zeros
+
+    """
+    dim = len(feature_vector)
+    mean_value = numpy.mean(feature_vector)
+    nearest_odd = utils.nearest_odd_root(dim)
+    op = trim_or_pad()
+    
+
+
+    pass
+
+
+def trim_or_pad(training_set, dim: int, nearest_odd: int, mode='default') -> Callable[[], numpy.ndarray]:
+    """
+    Decides if one should trim or pad the dataset's feature vector.
+
+    :param training_set:
+    :param dim:
+    :param nearest_odd:
+    :oaram mode: Which method we use to decide if trim or pad.
+                 'default' - 
+                 'only_trim' -
+                 'only_pad' -
+    
+    :return: The desired operation between 'trim' or 'pad' from the utils.
+    """
+    if mode == 'only_trim':
+        return utils.trim
+
+    if mode == 'only_pad':
+        return utils.pad
+    
+
+def get_feature_dimension(training_set):
+    """
     """
     pass
 
-def trim():
-    pass
--- TAC/utils.py
+++ TAC/utils.py
@@ -1,1 +1,46 @@
+import math
+import numpy
 
+
+def nearest_odd_root(d: int) -> int:
+    """
+    Computes the nearest odd s
+
+    TODO: make the code more clean, return sqrt_d + ((sqrt + 1) % 2) or something similar
+
+    :return: The nearest odd root from above. for example:
+             For d = 25 it will return 5.
+             For d = 26 it will return 7.
+    """
+    sqrt_d = math.sqrt(d)
+
+    if sqrt_d.is_integer():
+        if sqrt_d % 2 == 1:
+            # sqrt_d is odd
+            return int(sqrt_d)
+        else:
+            # sqrt_d is even
+            return int(sqrt_d + 1)
+    else:
+        # sqrt_d is not an integer
+        closet_int_from_above = int(sqrt_d) + 1
+        if closet_int_from_above % 2 == 1:
+            # is odd
+            return closet_int_from_above
+        else:
+            # is even
+            return closet_int_from_above + 1
+
+
+def pad(feature_vector: numpy.ndarray, k: int, method='random'):
+    """
+    Pads the feature vector
+    Methods: random noise, zeros
+    """
+    pass
+
+def trim(feature_vector: numpy.ndarray, k: int, method='random'):
+    """
+    Trim the feature_vector so 
+    """
+    pass
